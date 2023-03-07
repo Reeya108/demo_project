7c8d77a3f237a495f99778068a235162e4c713db
Manasee <rranade669@gmail.com>
2023-03-07 21:46:22 +0530

    all updated

---
+++ file_1.py
@@ -0,0 +1,15 @@
+"""
+
+this is demo file for clonning practice
+"""
+
+
+def addition(val):
+    if val == 30:
+        print("resolved submission")
+
+
+if __name__ == "__main__":
+
+    addition(20)
+
---
+++ file_2.py
@@ -0,0 +1,10 @@
+"""
+module level docstring
+"""
+
+
+def addition_all(a, b):
+    return "this is addition", a+b
+
+
+print(addition_all(10, 20))
